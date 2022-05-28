dc81255a04038779811aae4bd65f9dab8f87f4b2
zhangyajingvivian <105430047@qq.com>
2022-05-28 23:58:37 +0800

    Update WebUIBase.java

--- src/test/java/com/hogwarts/base/WebUIBase.java
+++ src/test/java/com/hogwarts/base/WebUIBase.java
@@ -53,9 +53,10 @@ public abstract class WebUIBase {
             System.setProperty("webdriver.chrome.driver", chromePath);
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("--headless");
-            chromeOptions.addArguments("-–no-sandbox");
+            chromeOptions.addArguments("--no-sandbox");
             //chromeOptions.addArguments("blink-settings=imagesEnabled=false");
-            chromeOptions.addArguments("-–disable-gpu");
+            chromeOptions.addArguments("--disable-gpu");
+            chromeOptions.addArguments("--disable-dev-shm-usage");
             driver = new ChromeDriver(chromeOptions);
         } else {
             System.setProperty("webdriver.gecko.driver", firefoxPath);
